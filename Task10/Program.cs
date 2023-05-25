6eb5d7660c1015efec437d699f4ce9cb7124e711
Alexander Lebyodkin <lav1ru@mail.ru>
2023-05-25 22:15:46 +0300

    add to Homework2

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,31 @@
-﻿Console.WriteLine("Введите трехзначное число");
-string input=Console.ReadLine();
-Console.WriteLine("{0}->{1}",input, input[1]);
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+while (true)
+{
+while (true)
+{
+    Console.Write("Введите трёхзначное число: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number))
+    {
+        if(number>99&&number<1000)
+        {int average=(number%100-number%10)/10;
+        Console.WriteLine($"Вторая цифра числа {number} является {average}");   
+            break;}
+    }
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+}
+Console.WriteLine("Будем получать среднюю цифру числа ещё? (1-Да, 0-Нет)");
+int d=Convert.ToInt32(Console.ReadLine());
+if (d==0)
+{
+    Console.Clear();
+    Console.SetCursorPosition(Console.WindowWidth / 10, Console.WindowHeight / 2);
+    Console.WriteLine("Если Вам не понравилась эта программа или она недостойна оплаты, хотя бы похвалите меня!");
+        break;
+};
+}
+
    
---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+ while (true)
+ {
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+ Console.WriteLine(k < 0 ? "Третьей цифры нет(((" : $"Третья цифра введённого числа {(n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString()}");
+Console.WriteLine("Будем пытаться получить третью цифру числа ещё раз? (1-Да, 0-Нет)");
+int d=Convert.ToInt32(Console.ReadLine());
+if (d==0)
+ {
+     Console.Clear();
+     Console.SetCursorPosition(Console.WindowWidth / 10, Console.WindowHeight / 2);
+     Console.WriteLine("Если Вам не понравилась эта программа или она недостойна оплаты, хотя бы похвалите меня!");
+         break;
+};
+}
+
+
+
+
+
+   
